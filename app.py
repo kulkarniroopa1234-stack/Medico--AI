cac8a8fdc2e111b7f8514d028536de71ee4cf2b9
kulkarniroopa1234-stack <kulkarniroopa1234@gmail.com>
2026-05-14 11:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Medico AI Backend Running"
+
+@app.route("/api/doctors")
+def doctors():
+    return jsonify({
+        "doctors": [
+            "Dr. Sharma",
+            "Dr. John",
+            "Dr. David"
+        ]
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
